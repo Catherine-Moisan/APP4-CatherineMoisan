2c7bbac4298867687a385f3a0a8ae884696c2e8b
2537441 <2537441@cegeplimoilou.ca>
2026-03-30 13:37:30 -0400

    correction d'une petite erreur

--- APP4/src/electronique/CircuitParallele.java
+++ APP4/src/electronique/CircuitParallele.java
@@ -1,6 +1,12 @@
 package electronique;
 
+import java.util.List;
+
 public class CircuitParallele extends Circuit {
+    public CircuitParallele(List<Composant> composants) {
+        super(composants);
+    }
+
     @Override
     public double calculerResistance() {
         return 0;
